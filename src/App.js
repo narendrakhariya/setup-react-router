6cca97fbf0dcb66df60ff1ac59bd72e71acded23
Narendra Khariya <khariyanaresh007@gmail.com>
2023-04-07 11:15:38 +0900

    Configure inital router in app  and use link to navigate pages in other component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+
+// 2nd approch old style
+// const routerDefination = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routerDefination);
+
+// 1st approch new style
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h1>My home page</h1>
+      <p>
+        {/* Below approch request the SPA index.html page to server and loads, problem in gloable state get refresh  */}
+        {/* Go to<a href="/products"> The list of products</a> */}
+
+        {/* Use link insted */}
+        <Link to="/products"> The list of products</Link>
+      </p>
+    </>
+  );
+};
+
+export default Home;
